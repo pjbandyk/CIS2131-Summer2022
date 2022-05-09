c1df1d5dc746745e50a45267625af6aa1cf97675
Eric Charnesky <eric.charnesky@careevolution.com>
2022-05-09 19:46:02 -0400

    added a bad joke

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print("Hello World!")
 print("I'm Eric!")
 print("Bob said \"This is fun\"")
+print("Here's some really long text, I'll a joke and you'll all roll your eyes at me, "
+      "I love data jokes....Did you hear the one about the pirate")
